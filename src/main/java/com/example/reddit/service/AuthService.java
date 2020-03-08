13ad63b9fa8115c8e9d41c8376c50eea0e33886f
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-03-08 17:34:05 +0530

    Added Code For generating Verification token

---
+++ src/main/java/com/example/reddit/service/AuthService.java
@@ -0,0 +1,49 @@
+package com.example.reddit.service;
+
+import com.example.reddit.dto.RegisterRequest;
+import com.example.reddit.model.User;
+import com.example.reddit.model.VerificationToken;
+import com.example.reddit.repository.UserRepository;
+import com.example.reddit.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
